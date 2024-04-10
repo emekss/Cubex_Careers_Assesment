19a262d74c6a8c9acc125a67148e8af5aa478a09
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-04-10 07:45:52 +0100

    registration screen updated

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,7 @@
-import 'package:cubex_careers/components/add_photo_button.dart';
+import 'dart:io';
 import 'package:cubex_careers/components/sign_up_button.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -10,6 +11,20 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  File? _image;
+
+  Future getImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+
+    if (image == null) return print('No image selected');
+
+    final imageTemporary = File(image.path);
+
+    setState(() {
+      _image = imageTemporary;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,9 +141,38 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ],
             ),
             const SizedBox(height: 10),
-            const Center(
-              child: AddPhotoButton(),
-            ),
+            Center(
+                child: GestureDetector(
+              onTap: () {
+                getImage(ImageSource.gallery);
+              },
+              child: Container(
+                height: 50,
+                width: 170,
+                decoration: BoxDecoration(
+                  color: Colors.green.shade800,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.add_a_photo_rounded,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'add photo',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )),
             const SizedBox(height: 10),
             const Center(
               child: SignUpButton(),
