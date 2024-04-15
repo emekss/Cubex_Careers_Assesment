d6d72ea022407692f5a194aa299ae553c9487111
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-04-15 08:57:31 +0100

    animation added to welcome screen

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
 import 'package:cubex_careers/components/sign_up_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -12,12 +11,31 @@ class RegistrationScreen extends StatefulWidget {
   State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen> {
+class _RegistrationScreenState extends State<RegistrationScreen>
+    with SingleTickerProviderStateMixin {
   TextEditingController userNameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController phoneNumberController = TextEditingController();
+
+  late AnimationController controller;
+  late Animation<Offset> slideTransition;
+  late List<Animation<Offset>> textFieldAnimation = [];
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    slideTransition =
+        Tween(begin: const Offset(-1, 1), end: Offset.zero).animate(controller);
+
+    controller.forward();
+  }
 
   void signUp(String username, email, password, phoneNumber, address) async {
     try {
@@ -71,105 +89,109 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ),
             ),
             const SizedBox(height: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Username',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-                TextField(
-                  controller: userNameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter username',
-                    hintStyle: TextStyle(color: Colors.grey),
+            SlideTransition(
+              position: slideTransition,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Username',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Email',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.lightGreen,
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter email',
+                  TextField(
+                    controller: userNameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter username',
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Password',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-                TextField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.lightGreen,
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter password',
-                    suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Email',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Phone Number',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-                TextField(
-                  controller: phoneNumberController,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.lightGreen,
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter phone number',
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.lightGreen,
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter email',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Address',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-                TextField(
-                  controller: addressController,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.lightGreen,
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter address',
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
                   ),
-                ),
-              ],
+                  TextField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.lightGreen,
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter password',
+                      suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Phone Number',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  TextField(
+                    controller: phoneNumberController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.lightGreen,
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter phone number',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Address',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  TextField(
+                    controller: addressController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.lightGreen,
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter address',
+                    ),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 10),
             const SizedBox(height: 10),
             Center(
               child: GestureDetector(
-                  onTap: () {
-                    signUp(
-                      userNameController.text.toString(),
-                      emailController.text.toString(),
-                      passwordController.text.toString(),
-                      phoneNumberController.text.toString(),
-                      addressController.text.toString(),
-                    );
-                  },
-                  child: const SignUpButton()),
+                onTap: () {
+                  signUp(
+                    userNameController.text.toString(),
+                    emailController.text.toString(),
+                    passwordController.text.toString(),
+                    phoneNumberController.text.toString(),
+                    addressController.text.toString(),
+                  );
+                },
+                child: const SignUpButton(),
+              ),
             ),
           ],
         ),
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,38 @@
+import 'package:cubex_careers/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> logoFadeAnimation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3000),
+    );
+    logoFadeAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    controller.forward();
+
+    logoFadeAnimation.addListener(() {
+      if (logoFadeAnimation.isCompleted) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: ((context) => const RegistrationScreen()),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +44,15 @@ class WelcomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
-              child: Text(
-                'CUBEX CAREERS',
-                style: TextStyle(
-                  color: Colors.green.shade800,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+              child: FadeTransition(
+                opacity: logoFadeAnimation,
+                child: Text(
+                  'CUBEX CAREERS',
+                  style: TextStyle(
+                    color: Colors.green.shade800,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
